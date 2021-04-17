e2d600ceb76eb801589afaf3adfa9cc3fd939aac
iamsaurabh007 <saurabh.yadav1947@gmail.com>
2021-04-17 20:00:56 +0530

    updated eval code for triplet loss version

--- ModelEval.py
+++ ModelEval.py
@@ -28,31 +28,27 @@ def get_ds(image, bounds):
     ds=[]
     coord=[]
     labels=[]
-    iouagg=[]
-    #wordid=[]
-    #seq=[]
-    bounds=bounds['iou']
+    wordid=[]
+    seq=[]
     for bound in bounds:
-        if not (bound['ground'] and bound['input']):
-            continue
         label=bound['text']
-        iou=bound['iou']
-        #wordid.append(bound['idword'])
-        #seq.append(bound['sequence'])
-        box = bound['input']['boundingBox']['vertices']
-        x_min=min(box[0]['x'],box[1]['x'],box[2]['x'],box[3]['x'])
-        x_max=max(box[0]['x'],box[1]['x'],box[2]['x'],box[3]['x'])
-        y_min=min(box[0]['y'],box[1]['y'],box[2]['y'],box[3]['y'])
-        y_max=max(box[0]['y'],box[1]['y'],box[2]['y'],box[3]['y'])
-        
-        im1 = image.crop((x_min,y_min,x_max,y_max))
+        wordid.append(bound['idword'])
+        seq.append(bound['sequence'])
+        bound = bound['boundingBox']
+        im1 = image.crop((bound["vertices"][0]['x'],
+                       bound["vertices"][0]['y'],
+                       bound["vertices"][2]['x'],
+                       bound["vertices"][2]['y']))
+
         ds.append((im1,label))
         labels.append(label)
-        iouagg.append(iou)
-        coord.append((x_min,y_min,x_max,y_max))
+        coord.append((bound["vertices"][0]['x'],
+                       bound["vertices"][0]['y'],
+                       bound["vertices"][2]['x'],
+                       bound["vertices"][2]['y']))
     
     #image.save(str(uuid.uuid1()) + '_handwritten.png')
-    return ds,coord,labels,iouagg
+    return ds,coord,labels,wordid,seq
 
 
 class IMGDS(torch.utils.data.Dataset):
@@ -132,7 +128,6 @@ if __name__ =='__main__':
     weight=[]
     mypath=join(config.pdfdata,"images")
     imgpaths = [join(mypath, f) for f in listdir(mypath) if isfile(join(mypath, f))]
-    imgpaths=[str for str in imgpaths if 'average' in str]
     refinement_ratio=[0.5]
     #refinement_ratio=[0.03,0.07,0.1,0.15,0.20,0.3,0.4,0.5]
     for ref in refinement_ratio:
@@ -140,49 +135,54 @@ if __name__ =='__main__':
         labelsagg=[]
         pageagg=[]
         predicagg=[]
-        iouagg=[]
+        wordidagg=[]
+        sequenceagg=[]
         page_list=[]
         for imgpath in imgpaths:
-            #with io.open(imgpath, 'rb') as image_file:
-            #    content = image_file.read()
-            jsonpath=config.pdfdata+"compare_json/"+os.path.splitext(os.path.basename(imgpath))[0]+".json"
+            with io.open(imgpath, 'rb') as image_file:
+                content = image_file.read()
+            jsonpath=config.pdfdata+"json/"+os.path.splitext(os.path.basename(imgpath))[0]+".json"
             with open(jsonpath) as f:
                 bounds = json.load(f)
-            #bounds=bounds_refine(bounds,imgpath,ref)
+            bounds=bounds_refine(bounds,imgpath,ref)
             #print("Characters in Image=",len(bounds))
-            ds,coords,labels,iou=get_ds(imgpath,bounds)
+            ds,coords,labels,wordid,seq=get_ds(imgpath,bounds)
             coordsagg.extend(coords)
             labelsagg.extend(labels)
             pageagg.extend([os.path.splitext(os.path.basename(imgpath))[0]]*len(labels))
-            iouagg.extend(iou)
+            wordidagg.extend(wordid)
+            sequenceagg.extend(seq)
             ds_train=IMGDS(label_dict,ds)
             train_gen = torch.utils.data.DataLoader(ds_train ,batch_size=64,shuffle=False,num_workers =6,pin_memory=True)
             train_gen =DataUtils.DeviceDataLoader(train_gen, device)
-            result = ModelUtils.evaluate(model,train_gen)
-            print("Accuracy on {} page is {}".format(imgpath,result['val_acc']))
-            pdf_acc.append(len(bounds)*result['val_acc'])
-            weight.append(len(bounds))
+           # result = ModelUtils.evaluate(model,train_gen)
             #os.remove(imgpath)
             #os.remove(jsonpath)
             train_gen = torch.utils.data.DataLoader(ds_train ,batch_size=64,shuffle=False,num_workers =6,pin_memory=True)
             train_gen =DataUtils.DeviceDataLoader(train_gen, device)
             predic=[]
+            batch_accs=[]
             for batch in train_gen:
                 images,labels= batch 
                 with torch.no_grad(): 
-                    out = model(images)
-                _, preds = torch.max(out, dim=1)
+                    out,_ = model(images)
+                    _, preds = torch.max(out, dim=1)
+                    batch_accs.append(ModelUtils.accuracy(preds,labels))
+                    val_acc=torch.stack(batch_accs).mean() 
                 predic.extend(preds.detach().cpu().numpy().tolist())
+            print("Accuracy on {} page is {}".format(imgpath,val_acc))
+            pdf_acc.append(len(bounds)*val_acc)
+            weight.append(len(bounds))
             predic=[revdict[i] for i in predic]
             predicagg.extend(predic)
             page_list.append(os.path.splitext(os.path.basename(imgpath))[0])
         df_main=pd.DataFrame(list(zip(page_list,pdf_acc,weight)),columns =['Page', 'Acc','Chars'])
-        df = pd.DataFrame(list(zip(coordsagg, labelsagg,predicagg,iouagg,pageagg)),\
-            columns =['Coordinates', 'Actual','Predicted','IOU','Page'])
-        csvpath=join(config.pdfdata,"csv/average/")
+        df = pd.DataFrame(list(zip(coordsagg, labelsagg,predicagg,wordidagg,sequenceagg,pageagg)),\
+            columns =['Coordinates', 'Actual','Predicted','Word','Sequence','Page'])
+        csvpath=join(config.pdfdata,"csv/")
         os.system('mkdir -p ' +csvpath)
-        csvpath2=join(csvpath,"MAINIncept161CRAFTkdeval.csv")
-        csvpath=join(csvpath,"DEATAILEDIncept161CRAFTkdeval.csv")
+        csvpath2=join(csvpath,"MAINIncept161RefineBinarykdeval.csv")
+        csvpath=join(csvpath,"DEATAILEDIncept161RefineBinarykdeval.csv")
         df.to_csv(csvpath,index=False)
         df_main.to_csv(csvpath2,index=False)
         print("ref={},   Accuracy Mean on this pdf is {}".format(ref,sum(pdf_acc)/sum(weight)))
