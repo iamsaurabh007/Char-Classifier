e6e91852df98e16e5a1dd6d5ef2c4d81e13b98a7
iamsaurabh007 <saurabh.yadav1947@gmail.com>
2021-04-25 15:25:58 +0530

    hyper gridsearch for finetuning

--- hypergridsearch.py
+++ hypergridsearch.py
@@ -3,5 +3,5 @@ from tqdm import tqdm
 
 if __name__ =='__main__':
     for i in tqdm([0.001,0.0001,0.000001,0.00001],desc="LR 3 4 6 5"):
-        for j in enumerate([16,4,64,256],desc="BS 16 4 64 256"):
-            run.RUN(j,i)
+        for j in tqdm([16,4,64,256],desc="BS 16 4 64 256"):
+            finetune.RUN(j,i)
