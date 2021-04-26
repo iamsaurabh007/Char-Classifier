908905965f8a596eb6ff7d5a6c3c99c171bc0194
iamsaurabh007 <saurabh.yadav1947@gmail.com>
2021-04-26 11:22:38 +0530

    changed grid search options

--- hypergridsearch.py
+++ hypergridsearch.py
@@ -2,6 +2,6 @@ import finetune
 from tqdm import tqdm
 
 if __name__ =='__main__':
-    for i in tqdm([0.001,0.0001,0.000001,0.00001],desc="LR 3 4 6 5"):
-        for j in tqdm([16,4,64,256],desc="BS 16 4 64 256"):
+    for i in tqdm([0.0001,0.00001,0.001],desc="LR 4,5,3"):
+        for j in tqdm([16,32],desc="BS 16 32,64"):
             finetune.RUN(j,i)
