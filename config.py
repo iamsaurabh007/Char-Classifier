e2d5e3f1926a709a96b7de3e8afcb2bdf35bab49
iamsaurabh007 <saurabh.yadav1947@gmail.com>
2021-04-28 10:02:01 +0530

    testing code

--- config.py
+++ config.py
@@ -26,5 +26,15 @@ channel=32
 ####MODELEVALUATION
 
 #checkpath="/home/ubuntu/data/ocr/ModelResnetfinal/epoch-36.pt"
-checkpath="/home/ubuntu/data/ocr/InceptFCByParts/CONVPART/epoch-20.pt"
+conv_checkpath="/home/ubuntu/data/ocr/InceptFCByParts/CONVPART/epoch-20.pt"
 pdfdata="/home/ubuntu/craft_benchmark/"
+
+
+
+
+########TESTING
+
+weightfilepath="/home/ubuntu/data/ocr/FC_PART/"
+testfiles=["good","bad","average"]
+testpath="/home/ubuntu/craft_benchmark/"
+testweights=["fc-epoch-16.pt","fc-epoch-103.pt","fc-epoch-8.pt","fc-epoch-277.pt"]
---
+++ test.py
@@ -0,0 +1,89 @@
+"""
+On list of model weights,
+On list of files folder,
+"""
+import io
+import sys 
+import os
+#import cv2
+import numpy as np
+import pandas as pd
+#import matplotlib.pyplot as plt
+#import glob
+from PIL import Image, ImageDraw
+import config
+import ModelUtils
+import Resnet
+import DataUtils
+import InceptFC
+
+#import pdf2image
+from os import listdir
+from os.path import isfile, join
+import torchvision
+import json
+import torch
+import utils
+from bounds_refinement import bounds_refine
+from tqdm import tqdm
+from torch.utils.tensorboard import SummaryWriter
+import random
+
+
+
+if __name__ =='__main__':
+    device=config.device
+    if device==None:
+        device = utils.get_default_device()
+    label_dict=utils.create_label_dict(config.symbols)
+    revdict={}
+    for i,sym in enumerate(config.symbols):
+        revdict[i]=sym
+
+    convmodel=InceptFC.Convolutional_block()()
+    convmodel.to(device)
+    checkpoint=torch.load(config.conv_checkpath, map_location=device)
+    convmodel.load_state_dict(checkpoint['model_state_dict'])
+    convmodel.train()
+    for model_wt in config.testweights:  
+        densemodel=InceptFC.FC_block()    
+        #model=Resnet.ResNet50(3,97)
+        model.to(device)
+        checkpath=join(config.weightfilepath,model_wt)
+        print(checkpath)
+        checkpoint=torch.load(checkpath, map_location=device)
+        densemodel.load_state_dict(checkpoint['model_state_dict'])
+        print("MODEL LOADED")
+        densemodel.train()
+        for category in config.testfiles:      ####
+            print("Model Weight is ",model_wt)
+            print("Test file is ",category)  ##
+            myvalpath=config.testpath   ###
+            valpath=join(myvalpath,"images/")
+            valid_paths = [join(valpath, f) for f in listdir(valpath) if isfile(join(valpath, f))]
+            valid_paths=[str for str in valid_paths if category in str]   #####
+            pdf_acc=[]
+            weight=[]
+            for imgpath in valid_paths:
+                jsonpath=join(myvalpath,"compare_json/")+os.path.splitext(os.path.basename(imgpath))[0]+".json"
+                with open(jsonpath) as f:
+                    bounds = json.load(f)
+                #bounds=bounds_refine(bounds,imgpath,0.48)
+                #print("Characters in Image=",len(bounds))
+                ds=utils.get_ds_crafts(imgpath,bounds)
+                ds_train=DataUtils.EVALIMGDS(label_dict,ds)
+                train_gen = torch.utils.data.DataLoader(ds_train ,batch_size=64,shuffle=False,num_workers =6,pin_memory=True)
+                train_gen =DataUtils.DeviceDataLoader(train_gen, device)
+                batch_accs=[]
+                for batch in train_gen:
+                    images,labels= batch 
+                    with torch.no_grad(): 
+                        embed=convmodel(images)
+                        out=densemodel(embed.detach())
+                        batch_accs.append(ModelUtils.accuracy(out,labels))
+                val_acc=torch.stack(batch_accs).mean().item() 
+                pdf_acc.append(len(bounds)*val_acc)
+                weight.append(len(bounds))
+            print("Test Accuracy Mean on this pdf is {}".format(sum(pdf_acc)/sum(weight)))
+            print("/n")
+            ################################################################
